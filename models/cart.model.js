04a1b4fd00bf703b824000a5dd04c5a2cb2e336f
moff <illholdthemoff@gmail.com>
2022-05-05 21:24:16 -0400

    finished day 90 and some of 91

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,6 +1,5 @@
 class Cart {
   constructor(items = [], totalQuantity = 0, totalPrice = 0) {
-    // initializes it as an empty array
     this.items = items;
     this.totalQuantity = totalQuantity;
     this.totalPrice = totalPrice;
@@ -29,6 +28,28 @@ class Cart {
     this.items.push(cartItem);
     this.totalQuantity++;
     this.totalPrice += product.price;
+  }
+
+  updateItem(productId, newQuantity) {
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i];
+      if (item.product.id === productId && newQuantity > 0) {
+        const cartItem = { ...item };
+        const quantityChange = newQuantity - item.quantity;
+        cartItem.quantity = newQuantity;
+        cartItem.totalPrice = newQuantity * item.product.price;
+        this.items[i] = cartItem;
+
+        this.totalQuantity = this.totalQuantity + quantityChange;
+        this.totalPrice += quantityChange * item.product.price;
+        return { updatedItemPrice: cartItem.totalPrice };
+      } else if (item.product.id === productId && newQuantity <= 0) {
+        this.items.splice(i, 1); // removes the item freom the cart if there is less than 1 of its kind in there.
+        this.totalQuantity = this.totalQuantity - item.quantity;
+        this.totalPrice -= item.totalPrice; // subtracts the price of the current item from that of the entire cart.
+        return { updatedItemPrice: 0 };
+      }
+    }
   }
 }
 
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -8,4 +8,6 @@ router.get("/", cartController.getCart); // effectively /cart/ due to our filter
 
 router.post("/items", cartController.addCartItem); // /cart/items
 
+router.patch("/items", cartController.updateCartItem);
+
 module.exports = router;
