d9d694dd1bf4a43a13ac4507b03f0454649aeb36
dowens <dowens@smartapp.com>
2022-05-25 17:38:20 -0400

    finished most day 93

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,5 +1,9 @@
+const stripe = require("stripe");
+
 const Order = require("../models/order.model");
 const User = require("../models/user.model");
+
+const stripeObj = stripe("sk_test_51L3REDHLAsqlklk1zMfUfklA0VW3KwqkZNRFGP1heYbfAWA4Z1VQbFcdWUOOeNesS0dmMRlxbkupxNorZ3PPWf3Q00cTxVLROy")
 
 async function getOrders(req, res) {
   try {
@@ -31,10 +35,43 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
-  res.redirect("/orders");
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: [
+      "card",
+    ],
+    line_items: [
+      {
+        // this is where you could enter in the entry of your product from stripe's database for your store. In this case hwoeever, we enter it in on the fly.
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: "Dummy"
+          },
+          unit_amount_decimal: 10.99,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `localhost:3000/orders/success`, // these 2 URLS redirect the user depending on the success or failure of the transaction
+    cancel_url: `localhost:3000/orders/cancel`,
+  });
+  res.redirect(303, session.url)
+
+  // res.redirect("/orders");
+}
+
+function getSuccess(req, res) {
+  res.render("customer/orders/success");
+}
+
+function getFailure(req, res) {
+  res.render("customer/orders/failure");
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure,
 };
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { get } = require("express/lib/response");
 
 const ordersController = require("../controllers/orders.controller");
 
@@ -7,5 +8,9 @@ const router = express.Router();
 router.post("/", ordersController.addOrder);
 
 router.get("/", ordersController.getOrders); // /orders
+
+router.get("/success", ordersController.getSuccess);
+
+router.get("/failure", ordersController.getFailure);
 
 module.exports = router;
